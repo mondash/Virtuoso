09dc7aa0c93a024d2d1a256012b253392aca86fc
Matthew Ondash <ondashm@gamil.com>
2025-07-29 15:04:15 -0500

    Extend BuglePitchFrame constructors

--- FooPlugin42/src/FooPlugin42/BuglePitch/BuglePitchFrame.cs
+++ FooPlugin42/src/FooPlugin42/BuglePitch/BuglePitchFrame.cs
@@ -2,15 +2,23 @@ using UnityEngine;
 
 namespace FooPlugin42.BuglePitch;
 
-internal readonly struct BuglePitchFrame(BugleSFX bugle)
+internal readonly struct BuglePitchFrame(int valves, float harmonic, float bend)
 {
-    public readonly int Valves = TrumpetValves.GetOffset();
-    public readonly float Harmonic = BuglePitchInput.GetHarmonic(bugle);
-    public readonly float Bend = BuglePitchInput.GetBend(bugle);
+    public readonly int Valves = valves;
+    public readonly float Harmonic = harmonic;
+    public readonly float Bend = bend;
+
+    public BuglePitchFrame(BugleSFX bugle) :
+        this(
+            TrumpetValves.GetOffset(),
+            BuglePitchInput.GetHarmonic(bugle),
+            BuglePitchInput.GetBend(bugle)
+        ) { }
+
     public float Semitone => Valves + Harmonic + Bend;
     public float Pitch => BuglePitchMath.ConvertToPitch(Semitone);
-    public bool Approximately(BuglePitchFrame other, float bendThreshold = 0.05f) =>
-        Valves == other.Valves
+    public bool Approximately(BuglePitchFrame other, float bendThreshold = 0.05f)
+        => Valves == other.Valves
         && Mathf.Approximately(Harmonic, other.Harmonic)
         && Mathf.Abs(Bend - other.Bend) < bendThreshold;
 }
